e1269f62eb23de1b6669e09322f3632ca0496e5d
ryuga0719 <ryuga.private@gmail.com>
2021-11-16 20:57:23 +0900

    initial commit

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+/** @format */
+
+module.exports = {
+    extends: ["stylelint-config-recommended", "stylelint-config-recommended-scss"],
+    rules: {
+        "color-hex-case": "lower",
+        "color-hex-length": "short",
+        "no-descending-specificity": null,
+        "no-duplicate-selectors": null,
+    },
+};
